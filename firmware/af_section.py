1690fb47e573835aaed8692fcf0837439ff56eaf
Jon Dawson <chips@jondawson.org.uk>
2021-02-25 21:04:42 +0000

    Make sure that the oscillator phase is not modulated when receiving

--- firmware/af_section.py
+++ firmware/af_section.py
@@ -181,18 +181,9 @@ def af_section(clk, rx_i, rx_q, rx_stb, tx_audio, tx_audio_stb, settings, debug=
     #output to transmitter
     #=====================
 
-    #in receive mode, send a test tone to the transmitter.
-    #if the test tone is enabled in the transmitter, the tone
-    #is swept at the receive frequency and can be used as a crude
-    #analyser.
-    #test_i, test_q, test_stb = test_signal(clk, tx_audio_stb)
-    #tx_mag = tx_mag.subtype.select(settings.rx_tx, test_i, tx_mag)
-    #tx_phase = tx_phase.subtype.select(settings.rx_tx, test_q, tx_phase)
-
-    #resize tx clamp if necessary
-    #tx_mag, stb   = clamp(clk, tx_mag, tx_stb, 8)
-    #tx_phase, _   = clamp(clk, tx_q, tx_stb, 8)
-    #tx_stb = stb
+    #Don't modulate the LO when receiving!!
+    tx_mag = tx_mag.subtype.select(settings.rx_tx, tx_mag.subtype.constant(0), tx_mag)
+    tx_phase = tx_phase.subtype.select(settings.rx_tx, tx_phase.subtype.constant(0), tx_phase)
 
     return rx, rx_stb, rx_USB, rx_USB_stb, tx_mag, tx_phase, tx_stb, power, capture_i, capture_q, capture_stb, overflow
 
